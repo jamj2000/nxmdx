524c88513cdce0f92ac5c943d340fcb07dc66b99
José Antonio Muñoz Jiménez <jamj2000@gmail.com>
2023-11-14 13:18:30 +0100

    ...

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
+const withMDX = require("@next/mdx")({
+    extension: /\.mdx?$/,
+    options: {
+      remarkPlugins: [require("remark-prism")],
+    },
+  });
+ 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+  // Configure `pageExtensions`` to include MDX files
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  // Optionally, add any other Next.js config below
+}
+ 
+module.exports = withMDX(nextConfig)
